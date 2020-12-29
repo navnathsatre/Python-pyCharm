4bd6bcdd8369126fa870a5282f28eaf5359e779f
navnathsatre <75266852+navnathsatre@users.noreply.github.com>
2020-12-29 22:48:48 +0530

    Add files via upload

---
+++ demo1.py
@@ -0,0 +1,16 @@
+print(10+20)
+var1 = 12
+var2 =23
+var3=var1+var2
+mul=var1*var2
+div=var1/var2
+sub=var1-var2
+print("Multi is -",mul)
+print("division is -%.3f"%(div))
+print("substraction is ",sub)
+
+print("Addition is ",var3)
+
+"""print(10 * "welcome in sos\n")
+
+print("welcome in my python programming")"""
