3448161faba6ef8990b8a0987fd785e52f00326b
navnathsatre <75266852+navnathsatre@users.noreply.github.com>
2021-01-04 12:25:17 +0530

    Add files via upload

---
+++ SqCube.py
@@ -0,0 +1,8 @@
+A=int(input("Take any number A is  "))
+B=int(input("Also take any number B is  "))
+square=A*A
+cube=A*A*A
+sq=B*B
+cub=B**3
+print(f"squar of A is {square} and cube of A is {cube}")
+print(f"squar of B is {sq} and cube of B is {cub}")
