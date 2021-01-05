f0bc4e1de74164e4033511763e605c2ccad403a5
navnathsatre <75266852+navnathsatre@users.noreply.github.com>
2021-01-05 14:23:01 +0530

    Add files via upload

---
+++ StringSlicing.py
@@ -0,0 +1,25 @@
+"""str=input("take any string :-")
+print(str[2:10:2]) #slicing the string
+print(str[::-1]) #reverse the string
+print(len(str)) #find length of string
+a,b,c=input("enter value").split('')
+print(a,b,c) #more than one vareable can split by , . *
+
+a="hello,worid"
+print(a)
+b=a.split(',')
+print(b) """
+
+string="iJoinedAPTECHlearninginstitute"
+print(string.title())
+print(string.upper())
+print(string.lower())
+print(string.find("learning"))
+print(string.count("u"))
+print(string.capitalize())
+print(string.replace("i","*"))
+print(string.lstrip())
+print(string.rstrip())
+print(string.strip())
+print(string.center(40,"*"))
+
